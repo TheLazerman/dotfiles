aa58961f4af810e7ba98b9337fff8feb0c01bc79
Gabe Coones <{ID}+{username}@users.noreply.github.com>
2024-07-17 16:30:32 -0500

    Testing iterm profile stuff

--- install.sh
+++ install.sh
@@ -2,12 +2,13 @@
 
 echo "[install] INFO: Installation Started"
 
+# Create the symlinks for our dotfiles
+~/dotfiles/create_symlink.sh
+
+
 # If were running on macos, do the thing for macos..
 if [[ "$(uname)" == "Darwin" ]]; then
     ~/dotfiles/macos.sh
 fi
-
-# Create the symlinks for our dotfiles
-~/dotfiles/create_symlink.sh
 
 echo "[install] INFO: Installation Finished"
--- macos.sh
+++ macos.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 echo "[macos] INFO: Installation Started"
+
 mkdir "$HOME/Documents/Development"
+
 ~/dotfiles/brew.sh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+if [ ! $HOME/.oh-my-zsh ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
 ln -sf "$HOME/dotfiles/files/com.googlecode.iterm2.plist" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
+ln -sf "$HOME/dotfiles/files/DefaultProfile.json" "$HOME/.config/iterm/DefaultProfile.json"
+
 ~/dotfiles/macos_config.sh
+
+exec zsh
+
 echo "[macos] INFO: Installation Complete"
