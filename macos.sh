3c52a46fc59f16e3ca6193df92bbb07dddb341a1
Gabe Coones <gcoones@THUNDERCHILD.local>
2024-07-17 17:02:29 -0500

    Dynamic Profiles?

--- macos.sh
+++ macos.sh
@@ -10,8 +10,8 @@ if [ ! $HOME/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
-ln -sf "$HOME/dotfiles/files/com.googlecode.iterm2.plist" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
-ln -sf "$HOME/dotfiles/files/DefaultProfile.json" "$HOME/.config/iterm/DefaultProfile.json"
+ln -sf "$HOME/dotfiles/files/com.googlecode.iterm2.plist" "$HOME/Library/Application Support/iTerm2/DynamicProfiles/com.googlecode.iterm2.plist"
+# ln -sf "$HOME/dotfiles/files/DefaultProfile.json" "$HOME/.config/iterm/DefaultProfile.json"
 
 ~/dotfiles/macos_config.sh
 
