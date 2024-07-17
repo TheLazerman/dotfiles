659c13e6f23ae23ed4f7b77f41804bc58bb8748a
Gabe Coones <{ID}+{username}@users.noreply.github.com>
2024-07-17 16:05:01 -0500

    Whoops, make the Development dir inside the Documents dir.

--- macos.sh
+++ macos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "[macos] INFO: Installation Started"
-mkdir "$HOME/Development"
+mkdir "$HOME/Documents/Development"
 ~/dotfiles/brew.sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 ln -sf "$HOME/dotfiles/files/com.googlecode.iterm2.plist" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
